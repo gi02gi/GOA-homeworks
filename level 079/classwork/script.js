d543195fe3f72dcaa59504ffd023c279000e4549
gi02gi <giorgichokheli209@gmail.com>
2026-01-24 17:03:44 +0400

    commit

---
+++ level 079/classwork/script.js
@@ -0,0 +1,106 @@
+let monthNumber = 5;
+if(monthNumber === 1){ 
+    console.log("იანვარი")
+}else if(monthNumber === 2){
+    console.log("თებერვალი")
+}else if(monthNumber === 3) {
+    console.log("მარტი")
+}else if(monthNumber === 4) {
+    console.log("აპრილი")
+}else if(monthNumber === 5) {
+    console.log("მაისი")
+}else if(monthNumber === 6) {
+    console.log("ივნისი")
+}else if(monthNumber === 7) {
+    console.log("ივლისი")
+}else if(monthNumber === 8) {
+    console.log("აგვისტო")
+}else if(monthNumber === 9) {
+    console.log("სექტემბერი")
+}else if(monthNumber === 10) {
+    console.log("ოქტომბერი")
+}else if(monthNumber === 11) {
+    console.log("ნოემბერი")
+}else if(monthNumber === 12) {
+    console.log("დეკემბერი")
+}else {
+    console.log("არასწორი რიცხვი")
+}
+
+let x = 5;
+console.log(
+    x === 1 ? "იანვარი" :
+    x === 2 ? "თებერვალი" :
+    x === 3 ? "მარტი" :
+    x === 4 ? "აპრილი" :
+    x === 5 ? "მაისი" :
+    x === 6 ? "ივნისი" :
+    x === 7 ? "ივლისი" :
+    x === 8 ? "აგვისტო" :
+    x === 9 ? "სექტემბერი" :
+    x === 10 ? "ოქტომბერი" :
+    x === 11 ? "ნოემბერი" :
+    x === 12 ? "დეკემბერი" :
+    "არასწორი რიცხვი"
+)
+
+let y = 5;
+
+switch(y) {
+    case 1: console.log("იანვარი"); break;
+    case 2: console.log("თებერვალი"); break;
+    case 3: console.log("მარტი"); break;
+    case 4: console.log("აპრილი"); break;
+    case 5: console.log("მაისი"); break;
+    case 6: console.log("ივნისი"); break;
+    case 7: console.log("ივლისი"); break;
+    case 8: console.log("აგვისტო"); break;
+    case 9: console.log("სექტემბერი"); break;
+    case 10: console.log("ოქტომბერი"); break;
+    case 11: console.log("ნოემბერი"); break;
+    case 12: console.log("დეკემბერი"); break;
+    default: console.log("არასწორი რიცხვი");
+}
+
+let dayNumber = 3;
+if(dayNumber === 1) {
+    console.log("ორშაბათი")
+}else if(dayNumber === 2) {
+    console.log("სამშაბათი")
+}else if(dayNumber === 3) {
+    console.log("ოთხშაბათი")
+}else if(dayNumber === 4) {
+    console.log("ხუთშაბათი")
+}else if(dayNumber === 5) {
+    console.log("პარასკევი")
+}else if(dayNumber === 6) {
+    console.log("შაბათი")
+}else if(dayNumber === 7) {
+    console.log("კვირა")
+}else {
+    console.log("არასწორი რიცხვი")
+}
+
+let o = 3;
+console.log(
+    0 === 1 ? "ორშაბათი" :
+    0 === 2 ? "სამშაბათი" :
+    0 === 3 ? "ოთხშაბათი" :
+    0 === 4 ? "ხუთშაბათი" :
+    0 === 5 ? "პარასკევი" :
+    0 === 6 ? "შაბათი" :
+    0 === 7 ? "კვირა" :
+    "არასწორი რიცხვი"
+)
+
+let xx = 3;
+switch(xx) {
+    case 1: console.log("ორშაბათი"); break;
+    case 2: console.log("სამშაბათი"); break;
+    case 3: console.log("ოთხშაბათი"); break;
+    case 4: console.log("ხუთშაბათი"); break;
+    case 5: console.log("პარასკევი"); break;
+    case 6: console.log("შაბათი"); break;
+    case 7: console.log("კვირა"); break;
+    default: console.log("არასწორი რიცხვი");
+}
