2d16739052557ab11228c968cb4a4ef19d4a61f8
gi02gi <giorgichokheli209@gmail.com>
2026-02-04 17:19:41 +0400

    commit

---
+++ level 086/classwork/script.js
@@ -0,0 +1,9 @@
+let names = ['daviti', 'eka', 'kate', 'nika', 'luka']
+let nums = [1, 2, 3, 4]
+names.push('giorgi')
+names.pop()
+names.join(' ')
+names.slice(0, 4)
+console.log(names)
+let a = names.concat(nums)
+
---
+++ level 086/homework/script.js
@@ -0,0 +1,14 @@
+const list = ['vashli', 'msxali', 'atami']
+list.push('bali')
+console.log(list)
+
+const nums = [10, 20, 30, 40]
+nums.pop()
+console.log(nums)
+
+const lists = ['vashli', 'msxali', 'atami', 'gio', 'eka']
+let a = lists.join('-')
+console.log(a)
+
+const x = ['vashli', 'msxali', 'atami', 'gio', 'eka', 'salome']
+console.log(x.slice(2,5))
