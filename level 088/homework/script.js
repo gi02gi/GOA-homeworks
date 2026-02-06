6b480bb51ed7f83a61ba971c94a2fc2c20514f94
gi02gi <giorgichokheli209@gmail.com>
2026-02-06 19:11:43 +0400

    commit

---
+++ level 088/homework/script.js
@@ -0,0 +1,33 @@
+for (let i = 0; i <= 38; i++){
+    console.log(i)
+}
+
+for (let i = 30; i < 40; i++){
+    console.log(i)
+}
+
+for (let i = 12; i < 120; i++){
+    console.log(i)
+}
+
+for (let i = 1; i <= 18; i++){
+    console.log('giorgi')
+}
+
+for (let i = 10; i < 100; i+=3){
+    console.log(i)
+}
+
+for (let i = 1; i <= 30; i++){
+    console.log(`${i} orange`)
+}
+
+x = 1
+while (x <= 10){
+    console.log(giorgi)
+}
+
+y = 0
+while (y < 24){
+    console.log(y)
+}
