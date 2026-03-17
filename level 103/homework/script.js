f2b1569b45cf0797c6c59dd02d918d5829d4da90
gi02gi <giorgichokheli209@gmail.com>
2026-03-17 21:12:58 +0400

    commit

--- level 103/homework/script.js
+++ level 103/homework/script.js
@@ -9,3 +9,15 @@ var uniqueInOrder=function(iterable){
     }
     return result;
 }
+
+
+function sortArray(array) {
+    let odds = array.filter(x => x % 2 !== 0).sort((a, b) => a - b);
+    let i = 0;
+    return array.map(x => {
+        if (x % 2 !== 0) {
+        return odds[i++];
+        }
+        return x;
+    });
+}
